38b3dd8e7287a905877c998e71d6419f0bdd3c6c
ppoum <pam@poum.ca>
2023-10-30 23:01:10 -0400

    Configure nodesource repo in stage0

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -2,6 +2,7 @@
 
 install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" > ${ROOTFS_DIR}/etc/apt/sources.list.d/nodesource.list
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
 
@@ -14,6 +15,11 @@ fi
 
 cat files/raspberrypi.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg"
 install -m 644 "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
+# Install nodesource key
+cat files/nodesource-repo.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/nodesource-repo.gpg"
+install -D -m 644 "${STAGE_WORK_DIR}/nodesource-repo.gpg" "${ROOTFS_DIR}/etc/apt/keyrings/nodesource.gpg"
+
 on_chroot << EOF
 dpkg --add-architecture armhf
 apt-get update
--- stage4/00-install-deps/00-run-chroot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-apt-get update
