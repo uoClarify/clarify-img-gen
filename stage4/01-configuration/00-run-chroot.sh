e7ae2a10780e40584180957152b58535090ab49d
ppoum <pam@poum.ca>
2023-11-09 10:43:24 -0500

    Auto login + change ownership of services

--- stage4/01-configuration/00-run-chroot.sh
+++ stage4/01-configuration/00-run-chroot.sh
@@ -2,5 +2,8 @@
 
 chown clarify:clarify /opt
 
-echo "i2c-dev" > /etc/modules
+echo "i2c-dev" >/etc/modules
 sed -i "s/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/" /boot/config.txt
+
+# Auto login into clarify account
+sed -i "s/#autologin-user=.*/autologin-user=clarify/" /etc/lightdm/lightdm.conf
