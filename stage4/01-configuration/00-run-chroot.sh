8d234d038dfc680a49457b84ec58a76f98f1cade
ppoum <pam@poum.ca>
2023-11-15 18:14:49 -0500

    Disable mouse cursor

--- stage4/01-configuration/00-run-chroot.sh
+++ stage4/01-configuration/00-run-chroot.sh
@@ -7,3 +7,6 @@ sed -i "s/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/" /boot/config.txt
 
 # Auto login into clarify account
 sed -i "s/#autologin-user=.*/autologin-user=clarify/" /etc/lightdm/lightdm.conf
+
+# Disable mouse cursor
+sed -i '/^\[Seat:\*\]/,$s/#xserver-command=X/xserver-command=X -bs -core -nocursor/' /etc/lightdm/lightdm.conf
