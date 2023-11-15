18e0faac4c4e9ca7aadc63dce7ae75623692cd4c
ppoum <pam@poum.ca>
2023-11-15 15:05:50 -0500

    Enable services + fix xorg problem for frontend service

--- stage4/02-apps/00-run.sh
+++ stage4/02-apps/00-run.sh
@@ -4,11 +4,11 @@ on_chroot <<EOF
 rm -dfr /opt/mirror-backend
 git clone https://${GH_USER}:${GH_PASS}@github.com/uoClarify/mirror-backend.git /opt/mirror-backend
 cd /opt/mirror-backend && pip install -r requirements.txt
-chown clarify:clarify /opt/mirror-backend
+chown -R clarify:clarify /opt/mirror-backend
 
 rm -dfr /opt/mirror-frontend
 git clone https://${GH_USER}:${GH_PASS}@github.com/uoClarify/mirror-app /opt/mirror-frontend
 cd /opt/mirror-frontend && npm install
-chown clarify:clarify /opt/mirror-frontend
+chown -R clarify:clarify /opt/mirror-frontend
 
 EOF
--- stage4/02-apps/01-run.sh
+++ stage4/02-apps/01-run.sh
@@ -3,3 +3,9 @@
 # Install files
 install -m 644 files/clarify-backend.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/clarify-frontend.service "${ROOTFS_DIR}/etc/systemd/system/"
+
+# Enable services
+on_chroot << EOF
+systemctl enable clarify-backend
+systemctl enable clarify-frontend
+EOF
