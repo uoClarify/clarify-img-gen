98635ca8bc9b2fd6657d89eb0949c1152b4b172f
ppoum <pam@poum.ca>
2023-11-15 21:04:44 -0500

    Use packaged frontend

--- stage4/02-apps/00-run.sh
+++ stage4/02-apps/00-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+install -m 644 files/frontend.zip "${ROOTFS_DIR}/opt/"
+
 on_chroot <<EOF
 rm -dfr /opt/mirror-backend
 git clone https://${GH_USER}:${GH_PASS}@github.com/uoClarify/mirror-backend.git /opt/mirror-backend
@@ -7,8 +9,9 @@ cd /opt/mirror-backend && pip install -r requirements.txt
 chown -R clarify:clarify /opt/mirror-backend
 
 rm -dfr /opt/mirror-frontend
-git clone https://${GH_USER}:${GH_PASS}@github.com/uoClarify/mirror-app /opt/mirror-frontend
-cd /opt/mirror-frontend && npm install
+mkdir -p /opt/mirror-frontend
+unzip /opt/frontend.zip -d /opt/mirror-frontend
+rm /opt/frontend.zip
+chmod +x /opt/mirror-frontend/clarify
 chown -R clarify:clarify /opt/mirror-frontend
-
 EOF
