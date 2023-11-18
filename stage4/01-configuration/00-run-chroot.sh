f4508f044113d6ae88ab736ca9eaf7971eb78658
ppoum <pam@poum.ca>
2023-11-18 16:01:19 -0500

    Auto-connect to WiFi on boot

--- stage4/01-configuration/00-run-chroot.sh
+++ stage4/01-configuration/00-run-chroot.sh
@@ -10,3 +10,9 @@ sed -i "s/#autologin-user=.*/autologin-user=clarify/" /etc/lightdm/lightdm.conf
 
 # Configure xserver (disable mouse cursor, screen blanking)
 sed -i '/^\[Seat:\*\]/,$s/#xserver-command=X/xserver-command=X -bs -core -nocursor -s 0 dpms/' /etc/lightdm/lightdm.conf
+
+# Configure WiFi (wpa_supplicant)
+wpa_passphrase VLaptop Clarify1! | tee -a /etc/wpa_supplicant/wpa_supplicant.conf
+cp /lib/systemd/system/wpa_supplicant.service /etc/systemd/system/wpa_supplicant.service
+sed -i "/^ExecStart/ s/$/ -i wlan0/" /etc/systemd/system/wpa_supplicant.service
+sed -i "/wlan0$/a Restart=always" /etc/systemd/system/wpa_supplicant.service
