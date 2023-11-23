8a984c8c6f14b2f2d7a5f73f7193fdf910c810ce
ppoum <pam@poum.ca>
2023-11-23 16:52:47 -0500

    Disabled audio jack + default to usb

--- stage4/01-configuration/00-run-chroot.sh
+++ stage4/01-configuration/00-run-chroot.sh
@@ -16,3 +16,10 @@ wpa_passphrase VLaptop Clarify1! | tee -a /etc/wpa_supplicant/wpa_supplicant.con
 cp /lib/systemd/system/wpa_supplicant.service /etc/systemd/system/wpa_supplicant.service
 sed -i "/^ExecStart/ s/$/ -i wlan0/" /etc/systemd/system/wpa_supplicant.service
 sed -i "/wlan0$/a Restart=always" /etc/systemd/system/wpa_supplicant.service
+
+# Disable snd_bcm2835 module
+echo "blacklist snd_bcm2835" > /etc/modprobe.d/snd-blacklist.conf
+
+# Set card 2 as default output (card0:hdmi0, card1:hdmi1, card2:usb)
+echo "options snd-usb-audio index=2" > /etc/modprobe.d/alsa-base.conf
+echo -e "defaults.pcm.card 2\ndefaults.ctl.card 2" > /etc/asound.conf
