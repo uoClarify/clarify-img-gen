1da17fc3ffd1d8cb86a39cff119891fa01772b89
ppoum <pam@poum.ca>
2023-11-28 22:33:57 -0500

    Added new variables

--- stage4/02-apps/02-run.sh
+++ stage4/02-apps/02-run.sh
@@ -9,4 +9,8 @@ cp /opt/mirror-backend/.env.default /opt/mirror-backend/.env
 # Picovoice key contains /, use | as delimiter
 sed -i "s|PICOVOICE_KEY=CHANGE|PICOVOICE_KEY=${PICOVOICE_KEY}|" /opt/mirror-backend/.env
 sed -i "s/GOOGLE_API_KEY=CHANGE/GOOGLE_API_KEY=${GOOGLE_API_KEY}/" /opt/mirror-backend/.env
+sed -i "s/SPOTIFY_CLIENT_ID=CHANGE/SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}/" /opt/mirror-backend/.env
+sed -i "s/SPOTIFY_CLIENT_SECRET=CHANGE/SPOTIFY_CLIENT_SECRET=${SPOTIFY_CLIENT_SECRET}/" /opt/mirror-backend/.env
+sed -i "s/OWM_API_KEY=CHANGE/OWM_API_KEY=${OWM_API_KEY}/" /opt/mirror-backend/.env
+sed -i "s/OPENAI_API_KEY=CHANGE/x=${x}/" /opt/mirror-backend/.env
 EOF
